a997819f7fb182aca61c053e142f3ca2cf2fa8e6
Hugo Mirault <miraulth@gmail.com>
2020-01-31 22:36:52 +0000

    ajout fichier de config

---
+++ Jeu/config.js
@@ -0,0 +1,20 @@
+//FICHIER DE CONFIG DU JEU TRON
+
+//MOTO
+const MOT_SIZEX = 11;
+const MOT_SIZEY = 20;
+const MOT_POSX = 200;
+const MOT_POSY = 100;
+
+
+//PLATEAU DE JEU
+const PL_L = 10;
+const PL_NBCOL =50;
+const PL_NBLIG =50;
+const MARGE = 10;
+
+
+//TEMPS DE JEU
+
+const INTERVAL = 100;
+const TMP_PARTIE = 20;// en seconde : 120 pour 2 minutes de jeu sur le plateau
