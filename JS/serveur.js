41b1a110e3fbe47d68f79f27b9d3ff92235e0dde
Thomas Besson <thomas.besson28@gmail.com>
2020-03-31 12:14:26 +0000

    mise à jour

--- JS/serveur.js
+++ JS/serveur.js
@@ -24,6 +24,14 @@ var Priority ;
 var pseudoPlayer1 = null;
 var pseudoPlayer2 = null;
 var id = 0;
+var motoMouv;
+var score = [0,0];
+
+var presD = 0; // pour la fct socket 'joueur_pret'
+var pres = 0; // pour la fct socket 'CommencerPartie'
+var Ppret = 0; //pour la fct socket 'ok_pret'
+var coll = 0;
+
 
 nsp.on('connection', function (socket) {
 
@@ -113,23 +121,6 @@ et sinon inclu*/
     console.log(Rooms);
     });
 
-    socket.on('CommencerPartie',(indiceRoom) =>{
-
-        console.log("Une Partie a commencé dans room " +indiceRoom);
-
-        if(id < 2){
-            id += 1;
-            socket.emit('id_joueur', id);
-        }
-        
-        if(id==2){
-        nsp.in(Rooms[indiceRoom].name).emit('joueurs_pret');
-        id = 0 ;
-        //socket.broadcast.emit('joueurs_pret');
-        }
-    });
-
-
     socket.on('EnvoiIdPartieAutreJoueur',(id,indiceRoom)=>{
         Rooms[indiceRoom].idPartie = id ;
         console.log("===============Envoi ID PArtie Autre Joueur =========\n");
@@ -138,24 +129,105 @@ et sinon inclu*/
         
     });
 
+    socket.on('demande_id',function(){
+        id = id+1;
+        socket.emit('id_joueur', id);
+    });
+   
+    socket.on('joueur_pret', function(){
+        presD += 1;
+        if(presD ==2) {
+            console.log("un des joueur est pret");
+            socket.emit('generer_partie');
+            presD = 0;
+        }
+        
+    });
+
+    socket.on('CommencerPartie',(indiceRoom) =>{
+        pres +=1;
+
+        if(pres==2){
+            //console.log("Une Partie a commencé dans room " +indiceRoom);
+            nsp.in(Rooms[indiceRoom].name).emit('lance_partie');
+            pres = 0;
+            coll = 0;
+        }
+    });
+
+   //lors d'une collision un message est envoyé au client même et à l'autre client
+    socket.on('collision',function(moto_id,indiceRoom){
+        coll += 1;
+        if(coll == 1){
+            clearInterval(motoMouv);
+            nsp.in(Rooms[indiceRoom].name).emit('fin_manche',moto_id);
+            nsp.in(Rooms[indiceRoom].name).emit('nouvelP','coucou');
+        }
+    });
 
     //quand un des clients est prêt on l'envoie à l'autre joueur
-    socket.on('envoi_autre_joueur_serveur', function(moto,indice){
-        console.log(indice);
-        console.log(moto);
+    socket.on('envoi_de_notre_moto', function(moto,indice){
+        //console.log(indice);
+        //console.log(moto);
         socket.to(Rooms[indice].name).emit('autre_joueur',moto);
     });
 
+    socket.on('ok_pret', function(IR, tmppartie, temp_refresh){
+        Ppret +=1;
+        if(Ppret == 2){
+            var seconde_left = 10;
+            var interval = setInterval(function(){
+
+                nsp.in(Rooms[IR].name).emit('decompte_avant_demarage_parti', seconde_left);
+
+                --seconde_left;
+
+                if(seconde_left <= 0){
+                    clearInterval(interval);
+                    TimerJeu(IR, tmppartie, temp_refresh);
+                    Ppret = 0;
+                }
+            }, 1000);
+        }
+        
+    });
     //si un joueur bouge on l'envoi à tout les clients
     socket.on('joueur_bouge', function(moto,indice){
         socket.to(Rooms[indice].name).emit('update_joueur', moto);
         //socket.broadcast.emit('update_joueur', moto);
     });
-   //console.log(nsp.adapter.rooms);
 
-   
+    socket.on('score', function(sc, id_joueur, indiceRoom){
+        score[id_joueur-1] = sc;
+        nsp.in(Rooms[indiceRoom].name).emit('vainceur',score[0], score[1]);
+    });
+
+
+   socket.on('envoiDePriorite',function(data){
+       console.log("this is data " + data );
+        Priority = data ;
+       console.log("this is data " +Priority);
+   });
+
+
     socket.on('disconnect',function(){
         console.log('disconnected');
     });
 
 });
+
+
+function TimerJeu(IR ,tempPartie, temp_refresh){
+    var second_ = tempPartie;
+    motoMouv = setInterval(function(){
+        nsp.in(Rooms[IR].name).emit('timer_manche_affichage' , second_/1000);
+        nsp.in(Rooms[IR].name).emit('frame');
+        second_ -= temp_refresh;
+
+        if(second_ <= 0){
+            clearInterval(motoMouv);
+            nsp.in(Rooms[IR].name).emit('fin_manche' , -1);
+            nsp.in(Rooms[IR].name).emit('nouvelP','coucou');
+        }
+    }, temp_refresh);
+}
