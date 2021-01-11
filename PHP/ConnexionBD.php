7481d2aeb3960cc8ec6597d6a7697e2312aa427a
momoDevDL <mohamed.masbah-abou-laich@master-developpement-logiciel.fr>
2021-01-11 21:59:43 +0100

    the start of refinig the webSite by establishing new dataBase Connection

--- PHP/ConnexionBD.php
+++ PHP/ConnexionBD.php
@@ -1,7 +1,10 @@
 <?php
- $psswd = "t5n8O9FMkWEb4ssP";
+ $psswd = "root";
+ $usr = "phpmyadmin";
 	try{
-		$dbh = new PDO("mysql:host=localhost;dbname=lightcyclefight;charset=UTF8",'debian-sys-maint',$psswd,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,));
+		
+		$dbh = new PDO("mysql:host=localhost;dbname=lightcyclefight;charset=UTF8",$usr,$psswd,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,));
+		
 	} catch(PDOException $e){
 		echo $e->getMessage();
 		die("Connexion impossible !");
--- PHP/creationCompte.php
+++ PHP/creationCompte.php
@@ -8,18 +8,18 @@
         $coulM = $_POST['colorM'];
         $coulG = $_POST['colorG'];  
         
-        
+       
             if(isset($_POST['Pseudo']) && isset($_POST['email']) && isset($_POST['passwd']) && isset($_POST['colorG']) && isset($_POST['colorM'])  ){
-                
+                 
                 $query = "SELECT PSEUDO FROM UTILISATEUR WHERE PSEUDO='$pseudo'";
                 $checkExistance = $dbh->query($query);
                 
                 if($checkExistance->rowCount() == 0 ){
-                $sql  = "INSERT INTO UTILISATEUR VALUES('$pseudo',md5('$passwd'),'JOUEUR','$email',1,100,0.99,NULL,'$coulG','$coulM','null','-wooden-pingpong-table-709134.jpg')";  
+                $sql  = "INSERT INTO UTILISATEUR VALUES('$pseudo',md5('$passwd'),'JOUEUR','mohamedmasbahaboulaich@gmail.com',0,0.99,NULL,1500,'$coulG','$coulM',0,'-wooden-pingpong-table-709134.jpg')";  
                 //$query = "INSERT INTO UTILISATEUR VALUES('$pseudo',md5($passwd),'JOUEUR','$email',1,100,1.00,'NULL','ORANGE','BLEU')";
                 
                 $req = $dbh->query($sql);
-                
+                echo "whaaaaaat";
                 session_start();
                 $_SESSION['id_utilisateur']= $pseudo;
                 $_SESSION['id_role']="JOUEUR";
@@ -34,4 +34,4 @@
         
         }
 
-?>  
+?>      
